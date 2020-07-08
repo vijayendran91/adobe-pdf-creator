1e92cd8c29ee650211f74a86229fbb28f9fc490f
vijayendran91 <vijayendran91@gmail.com>
2020-07-08 12:49:31 +0530

    Creating Ruby CLI gem pdf_creator

---
+++ pdf_creator/lib/pdf_creator/version.rb
@@ -0,0 +1,3 @@
+module PdfCreator
+  VERSION = "0.1.0"
+end
---
+++ pdf_creator/spec/pdf_creator_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe PdfCreator do
+  it "has a version number" do
+    expect(PdfCreator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
